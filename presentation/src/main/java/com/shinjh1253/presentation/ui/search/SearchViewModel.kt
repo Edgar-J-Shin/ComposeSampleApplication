48f83f2c474de9e6afaed5d9d02e58dd2a6e453d
edgarshin <edgar.shin@tosslab.com>
2024-12-15 15:06:13 +0900

    fix #13 searchbar event delegate 추가

--- presentation/src/main/java/com/shinjh1253/presentation/ui/search/SearchViewModel.kt
+++ presentation/src/main/java/com/shinjh1253/presentation/ui/search/SearchViewModel.kt
@@ -2,7 +2,6 @@ package com.shinjh1253.presentation.ui.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.map
 import com.shinjh1253.domain.usecase.GetBookmarksUseCase
@@ -10,6 +9,7 @@ import com.shinjh1253.domain.usecase.GetImagesUseCase
 import com.shinjh1253.domain.usecase.UpdateBookmarkUseCase
 import com.shinjh1253.presentation.core.state.SnackbarState
 import com.shinjh1253.presentation.core.ui.EventDelegate
+import com.shinjh1253.presentation.ui.component.searchbar.SearchbarEventDelegate
 import com.shinjh1253.presentation.model.DocumentUiState
 import com.shinjh1253.presentation.model.KeywordUiState
 import com.shinjh1253.presentation.model.SearchUiState
@@ -21,14 +21,12 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -40,7 +38,8 @@ class SearchViewModel @Inject constructor(
     private val getBookmarksUseCase: GetBookmarksUseCase,
 ) :
     ViewModel(),
-    EventDelegate<SearchUiEffect, SearchUiEvent> by EventDelegate.EventDelegateImpl() {
+    EventDelegate<SearchUiEffect, SearchUiEvent> by EventDelegate.EventDelegateImpl(),
+    SearchbarEventDelegate<SearchbarUiEvent> by SearchbarEventDelegate.SearchbarEventDelegateImpl(){
 
     private val coroutineExceptionHandler =
         CoroutineExceptionHandler { _, exception ->
@@ -82,11 +81,6 @@ class SearchViewModel @Inject constructor(
                     }
                 }
         }
-        .stateIn(
-            scope = viewModelScope,
-            initialValue = PagingData.empty(),
-            started = SharingStarted.WhileSubscribed(5_000)
-        )
 
     private fun updateSearchText(newText: String) {
         _searchUiState.update { it.copy(query = KeywordUiState(newText)) }
@@ -120,7 +114,7 @@ class SearchViewModel @Inject constructor(
         }
     }
 
-    fun dispatchSearchEvent(event: SearchbarUiEvent) {
+    override fun dispatchSearchbarEvent(event: SearchbarUiEvent) {
         when (event) {
             is SearchbarUiEvent.OnSearchTextChanged -> {
                 updateSearchText(event.query)
